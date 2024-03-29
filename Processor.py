e1f63ef8730172110040755a035784cb8cb33be9
Oleksii Troshchyi <oleksii.troshchyi@gr8.tech>
2024-03-29 15:55:58 +0200

    Create sum calculation

--- Processor.py
+++ Processor.py
@@ -1,25 +1,23 @@
 from tkinter import *
 
 
-def display_result_matrix(result_frame, result_matrix):
-
+def calculate_matrices_sum(matrix1, matrix2, result_frame):
     for widget in result_frame.winfo_children():
         widget.destroy()
 
-    for i, row in enumerate(result_matrix):
-        for j, value in enumerate(row):
-            Label(result_frame, text=f"{value:.2f}", width=5).grid(row=i, column=j)
-
-
-def calculate_and_display_matrices_sum(matrix1, matrix2, result_frame):
-    result_matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    result_matrix = []
     for i in range(3):
+        row = []
         for j in range(3):
             try:
                 value1 = float(matrix1[i][j].get())
                 value2 = float(matrix2[i][j].get())
-                result_matrix[i][j] = value1 + value2
+                row.append(value1 + value2)
             except ValueError:
-                pass
+                row.append(0)
+        result_matrix.append(row)
 
-    display_result_matrix(result_frame, result_matrix)
+    for i, row_values in enumerate(result_matrix):
+        for j, value in enumerate(row_values):
+            result_label = Label(result_frame, text=f"{value:.2f}", width=5)
+            result_label.pack(side=LEFT)
--- UI.py
+++ UI.py
@@ -1,23 +1,25 @@
 from tkinter import *
 from Column_UI import *
-from Processor import calculate_and_display_matrices_sum
+from Processor import calculate_matrices_sum
 
+# Створення вікна Tkinter
 window = Tk()
-window.title("AliceBob")
+window.title("Операції з матрицями")
 
+# Створення контейнера для матриць
 matrix_container = Frame(window)
 matrix_container.pack(side=TOP, fill=X)
-
-result_frame = Frame(window)
-result_frame.pack(side=TOP, fill=X, pady=(10, 0))
 
 frame1, label1, extra_entry1, matrix1 = create_labeled_matrix(matrix_container, "Alice")
 frame2, label2, extra_entry2, matrix2 = create_labeled_matrix(matrix_container, "Bob")
 
-buttons_container = Frame(window)
-buttons_container.pack(side=TOP, fill=X)
+# Створення фрейму для відображення результату в тому ж matrix_container
+result_frame = Frame(matrix_container)
+result_frame.pack(side=LEFT, padx=10, pady=10)
 
-calculate_button = Button(buttons_container, text="Calculate", command=lambda: calculate_and_display_matrices_sum(matrix1, matrix2, result_frame))
+# Додавання загальної кнопки під контейнером з матрицями, яка викликає функцію обрахунку
+calculate_button = Button(window, text="Calculate",
+                          command=lambda: calculate_matrices_sum(matrix1, matrix2, result_frame))
 calculate_button.pack(side=BOTTOM, fill=X)
 
 window.mainloop()
